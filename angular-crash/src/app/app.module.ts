fc9a35353726be90c5e91345d4a34757735416f6
GeorgeAnghel114 <georgeanghel114@gmail.com>
2023-04-11 12:21:56 +0300

    [feat] task & task-item component

--- angular-crash/src/app/app.module.ts
+++ angular-crash/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componenets/header/header.component';
 import { ButtonComponent } from './componenets/button/button.component';
+import { TasksComponent } from './componenets/tasks/tasks.component';
+import { TaskItemComponent } from './componenets/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
