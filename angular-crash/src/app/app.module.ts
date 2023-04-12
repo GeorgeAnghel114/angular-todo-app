2152d1937c1182d4bacf84f678e70f238abdebe6
GeorgeAnghel114 <georgeanghel114@gmail.com>
2023-04-12 11:45:46 +0300

    [feat] add task

--- angular-crash/src/app/app.module.ts
+++ angular-crash/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componenets/header/header.component';
 import { ButtonComponent } from './componenets/button/button.component';
 import { TasksComponent } from './componenets/tasks/tasks.component';
 import { TaskItemComponent } from './componenets/task-item/task-item.component';
+import { AddTaskComponent } from './componenets/add-task/add-task.component';
 
 @NgModule({
   declarations: [
@@ -14,12 +17,14 @@ import { TaskItemComponent } from './componenets/task-item/task-item.component';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule 
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
